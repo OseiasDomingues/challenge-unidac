d7ad3c2ee27f0063d16393c74147eea5ddaac5f7
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-11-13 15:16:58 -0300

    created models - Collaborator and Foods

---
+++ src/main/java/com/grupodl/models/Collaborator.java
@@ -0,0 +1,87 @@
+package com.grupodl.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="TB_Collaborator") 
+public class Collaborator implements Serializable {
+		
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String cpf;
+	private String name;
+	
+	@OneToMany(mappedBy = "collaborator")
+	private List<Food> foods = new ArrayList<>();
+	
+	
+		
+
+	public Collaborator() {
+	}
+
+	public Collaborator(String cpf, String name) {
+		this.cpf = cpf;
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Food> getFoods() {
+		return foods;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Collaborator other = (Collaborator) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+
+	@Override
+	public String toString() {
+		return "Collaborator [cpf=" + cpf + ", name=" + name + ", foods=" + foods + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/grupodl/models/Food.java
@@ -0,0 +1,81 @@
+package com.grupodl.models;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_Foods")
+public class Food {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_collaborator")	
+	private Collaborator collaborator;
+	
+	public Food() {
+	}
+	
+	public Food(Long id, String name, Collaborator collaborator) {
+		this.id = id;
+		this.name = name;
+		this.collaborator = collaborator;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collaborator getCollaborator() {
+		return collaborator;
+	}
+
+	public void setCollaborator(Collaborator collaborator) {
+		this.collaborator = collaborator;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Food other = (Food) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Food [id=" + id + ", name=" + name + ", collaborator=" + collaborator + "]";
+	}		
+
+}
